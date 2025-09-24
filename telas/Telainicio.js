07d1596e3787edf0bbeff156550e32f1c400708e
anabia278 <ana.cezar3@etec.sp.gov.br>
2025-09-23 22:19:00 -0300

    Criando telas

---
+++ telas/Telainicio.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function Telainicio() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Bem-vindo à Tela de Inicio</Text>
+        </View>
+    );
+}
+
+export default Telainicio;
